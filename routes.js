636ea8348ea0d1bc1313ada478803f9b67d8e2d1
JBloodBeardN <jtn.neese@gmail.com>
2019-04-26 20:04:26 -0500

    added handlebars render to route and #each loop

--- routes.js
+++ routes.js
@@ -5,8 +5,19 @@ var router = express.Router();
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+//homepage get route
 router.get("/", function(req, res){
-    res.render("scraper")
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.render("scraper", dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+    
 })
 // A GET route for scraping the echoJS website
 router.get("/scrape", function (req, res) {
